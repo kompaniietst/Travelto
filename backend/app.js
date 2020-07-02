ddd3df6c2cea88aacd1068564be9ae671764cb9c
kompaniietst <kompaniets.t@gmail.com>
2020-07-02 16:53:08 +0300

    .

--- backend/app.js
+++ backend/app.js
@@ -32,38 +32,6 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
-// app.listen(process.env.PORT, function () {
-//   console.log('***** exp listening on port: ' + process.env.PORT);
-// });
-
-
-
-// const express = require('express')
-// const bodyParser = require('body-parser');
-// require('./src/db/db')
-// const port = process.env.PORT;
-// const cors = require('cors');
-
-// const userRouter = require('./src/routers/user')
-// const User = require('./src/models/User')
-
-// const hotelRouter = require('./src/routers/hotel')
-// const Hotel = require('./src/models/Hotel')
-
-// const roomRouter = require('./src/routers/room')
-
-// const amenityRouter = require('./src/routers/amenity')
-// const Amenity = require('./src/models/Amenity')
-
-// const cityRouter = require('./src/routers/city')
-// const City = require('./src/models/City')
-
-// const bookingRouter = require('./src/routers/booking')
-// const Booking = require('./src/models/Booking')
-
-// const app = express()
-
-// app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(hotelRouter)
@@ -72,27 +40,26 @@ app.use(amenityRouter)
 app.use(cityRouter)
 app.use(bookingRouter)
 
-// app.use(cors());
 
 // app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(bodyParser.json())
 
 
-// var multer = require('multer')
+var multer = require('multer')
 
 
-// function uploadImages(folderName) {
-//     return multer({
-//         storage: multer.diskStorage({
-//             destination: function (req, file, cb) {
-//                 cb(null, `./uploads/${folderName}/`)
-//             },
-//             filename: function (req, file, cb) {
-//                 cb(null, (file.originalname).replace(/ /g, ''))
-//             }
-//         })
-//     })
-// }
+function uploadImages(folderName) {
+    return multer({
+        storage: multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, `./uploads/${folderName}/`)
+            },
+            filename: function (req, file, cb) {
+                cb(null, (file.originalname).replace(/ /g, ''))
+            }
+        })
+    })
+}
 
 // app.use('/images/hotels', express.static(__dirname + '/uploads/hotels'));
 // app.post('/images/hotels', uploadImages('hotels').array('files'), function (req, res, next) {
@@ -108,10 +75,10 @@ app.use(bookingRouter)
 //     res.send(files);
 // });
 
-// app.use('/images/amenities', express.static(__dirname + '/uploads/amenities'));
-// app.post('/images/amenities', uploadImages('amenities').array('files'), function (req, res, next) {
-//     res.send(req.files);
-// });
+app.use('/images/amenities', express.static(__dirname + '/uploads/amenities'));
+app.post('/images/amenities', uploadImages('amenities').array('files'), function (req, res, next) {
+    res.send(req.files);
+});
 
 // app.get('/healthcheck',async (req, res) => {
 //     res.send("Nope");
